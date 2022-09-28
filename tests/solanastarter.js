170b1ca7f2cb096644c62d6e4979d756e610ab86
jennyg0 <jen2348@hotmail.com>
2022-09-27 21:06:38 -0400

    store structs

--- tests/solanastarter.js
+++ tests/solanastarter.js
@@ -23,13 +23,20 @@ const main = async () => {
   let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
   console.log("👀 GIF Count", account.totalGifs.toString());
 
-  await program.rpc.addGif({
-    accounts: {
-      baseAccount: baseAccount.publicKey,
-    },
-  });
+  await program.rpc.addGif(
+    "https://media.giphy.com/media/ejJclNX60XyEo555wW/giphy.gif",
+    {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+      },
+    }
+  );
+
   account = await program.account.baseAccount.fetch(baseAccount.publicKey);
   console.log("👀 GIF Count", account.totalGifs.toString());
+
+  console.log("👀 GIF List", account.gifList);
 };
 
 const runMain = async () => {
