0dbf7ef7d3c3d5ee758908a22ad7da598dae619a
jennyg0 <jen2348@hotmail.com>
2022-09-28 12:37:21 -0400

    fix naming

--- programs/solanastarter/src/lib.rs
+++ programs/solanastarter/src/lib.rs
@@ -3,7 +3,7 @@ use anchor_lang::prelude::*;
 declare_id!("B85YcqVUBMK1Ba7K2jg7xmwXadJEMrRD6NKEXUdokeYc");
 
 #[program]
-pub mod yourprojectnamehere {
+pub mod solanastarter {
   use super::*;
   pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result <()> {
     let base_account = &mut ctx.accounts.base_account;
